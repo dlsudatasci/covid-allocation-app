8a4d29191aae1abaf317a20161ba90b8ac303093
fredtapia <74693862+fredtapia@users.noreply.github.com>
2021-02-01 15:28:54 +0800

    multiple vaccines
    Added multiple vaccine feature

--- VACModel.py
+++ VACModel.py
@@ -14,57 +14,99 @@ class VACModel():
 
     def __init__(self):
         #initial data
-        self.Group = ["A", "B", "C", "D", "E"]
-        self.N0= {"A":77, "B":241, "C":375, "D":204, "E":103}
-        self.fn0= {"A":0.5, "B":0.5, "C":0.5, "D":0.5, "E":0.5}
+        self.Group = ["A", "B", "C", "D", "E"] #groups
+        self.N0= {"A":77, "B":241, "C":375, "D":204, "E":103} #population per group
+        #self.fn0= {"A":0.5, "B":0.5, "C":0.5, "D":0.5, "E":0.5} #default values for allocation variables
+        self.vac_type = ["Vac A", "Vac B"] #vaccine types
         self.Kmatval = [
             0.6, 0.1, 0.1, 0.1, 0.1,
             0.2, 1.7, 0.3, 0.2, 0.2,
             0.4, 0.3, 0.5, 0.4, 0.3,
             0.2, 0.1, 0.3, 0.2, 0.1,
             0.1, 0.1, 0.1, 0.1, 0.1
-            ]
-        self.H = 0.98
+            ] #contact rates between groups
+        self.H = [0.98, 0.90] #vaccine efficacy values
+        self.vac_avail = [30, 100] #available number of vaccines
+        self.Hvac = dict(zip(self.vac_type, self.H)) #dictionary used for the model
+        self.model_sub_type = "minR" #see below
+        
+        """
+        subtype of model
+         - minR - minimize spread of infection, measured by R - reproductive number
+         - minV - minimize vaccines required, measured by V - vaccine requirements
+        """
 
     def run(self):
         Group = self.Group
         N0 = self.N0
-        fn0 = self.fn0
+        #fn0 = self.fn0
         Kmatval = self.Kmatval
-        H = self.H
+        Hvac = self.Hvac
+        vac_type = self.vac_type
+        vac_avail = self.vac_avail
 
         vac = ConcreteModel()
         vac.g = Set(initialize=Group)
         vac.mat = vac.g *vac.g
+        vac.vtype = Set(initialize=vac_type)
+        vac.gvtype = vac.g * vac.vtype
         
         Kmat00 = dict(zip([ij for ij in vac.mat],Kmatval))
         
         #parameters
         vac.K = Param(vac.mat, initialize=Kmat00, mutable=True)
         vac.n = Param(vac.g, initialize=N0)
+        vac.H = Param(vac.vtype, initialize = Hvac)
+        vac.avail = Param(vac.vtype, initialize = dict(zip(vac_type,vac_avail)))
         
         #variables
-        vac.f = Var(vac.g, initialize=fn0, within=NonNegativeReals)
-        vac.v = Var(vac.g, initialize=fn0, within=NonNegativeReals)
+        vac.f = Var(vac.gvtype, within=NonNegativeReals) # percent of available vaccine to 
+        vac.v = Var(vac.g, within=NonNegativeReals) # values in the eigenvector - determines R
+        vac.R = Var(within=NonNegativeReals) #reproductive number
         
         #objective function
         def OBF(vac):
-            return sum(vac.n[i]*vac.f[i] for i in vac.g)
+            if self.model_sub_type == "minV":
+                return sum(vac.n[i]*vac.f[i,p] for i in vac.g for p in vac.vtype)
+            elif self.model_sub_type == "minR":
+                return vac.R
         vac.obj = Objective(rule=OBF, sense=minimize)
         
         #constraints
         def Co1(vac):
-            return sum(vac.v[i] for i in vac.g) == 1
+            return sum(vac.v[i] for i in vac.g) == vac.R
         vac.C1 = Constraint(rule=Co1)
         
+        if self.model_sub_type == "minR":
+            def Co2_minR(vac, p):
+                return sum(vac.f[i,p] for i in vac.g) <= vac.avail[p]
+            vac.C2p = Constraint(vac.vtype, rule=Co2_minR)        
+        
         def Co2(vac, i):
-            return (1-H*vac.f[i])*sum(vac.K[i,j]*vac.v[j] for j in vac.g) <= vac.v[i]
+            return (1-sum(vac.H[p]*vac.f[i,p] for p in vac.vtype))*sum(vac.K[i,j]*vac.v[j] for j in vac.g) <= vac.R*vac.v[i]
         vac.C2 = Constraint(vac.g, rule=Co2)
         
+        def Co3(vac, i, p):
+            return vac.f[i,p] <= 1
+        vac.C3 = Constraint(vac.gvtype, rule=Co3)
+
+        def Co4(vac,i):
+            return sum(vac.f[i,p] for p in vac.vtype) <=1
+        vac.C4 = Constraint(vac.g, rule= Co4)
         
         SolverFactory('ipopt').solve(vac)
-        return [vac.f[j].value for j in Group], [vac.v[j].value for j in Group]
+        vac.pprint()
+        return vac.R.value
+        #return [vac.f[j].value for j in Group], [vac.v[j].value for j in Group]
 
+
+#test
+if __name__ == "__main__":
+    model = VACModel();
+    result = model.run()
+    print(result)
+    
+    
 """
 Answer = []
 
